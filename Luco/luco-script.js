9b7486c2a1cf8751483bb947db4a2e01df83b37a
Matt Albinson <mattalbinson@berkeley.net>
2017-05-24 10:23:34 -0700

    something2

--- Luco/luco-script.js
+++ Luco/luco-script.js
@@ -1,3 +1,25 @@
 /**
  * Created by h205p2 on 5/18/17.
  */
+
+function User(name, pass, sex, weight, height) {
+    this.username = name;
+    this.password = pass;
+    this.sex = sex;
+    this.weight = weight;
+    this.height = height;
+    this.bmiheight = this.height * this.height;
+    this.bmi = (this.weight / this.bmiheight) * 703
+}
+
+function build() {
+    $(document).ready(function(){
+        var user1 = new User(
+            $("#username2").val(),
+            $("#password2").val(),
+            $("#select-choice-min").val(),
+            $("#weight").val(),
+            $("#height").val());
+        console.log(user1);
+    });
+}
