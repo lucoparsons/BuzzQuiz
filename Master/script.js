9447dd7cbff1ed48673136f1c3a981f2e74f6eb7
Borna Karimi <bornakarimi@students.berkeley.net>
2017-05-25 09:55:33 -0700

    master

--- Master/script.js
+++ Master/script.js
@@ -1,3 +1,30 @@
-/**
- * Created by h205p2 on 5/18/17.
- */
+$(".ui-btn .ui-corner-all .ui-btn-inline").css("background-color", "#DD3355");
+
+function User(name, pass, sex, weight, height) {
+    this.username = name;
+    this.password = pass;
+    this.sex = sex;
+    this.weight = weight;
+    this.height = height;
+    this.bmiheight = this.height * this.height;
+    this.bmi = (this.weight / this.bmiheight) * 703
+}
+
+function build() {
+    $(document).ready(function(){
+        user1 = new User(
+            $("#username2").val(),
+            $("#password2").val(),
+            $("#select-choice-min").val(),
+            $("#weight").val(),
+            $("#height").val());
+        console.log(user1);
+    });
+    function howmuchshouldyoudrink(){
+        var liquidLeft = user1.weight * .6;
+        console.log(liquidLeft);
+    }
+    howmuchshouldyoudrink();
+}
+
+build();
