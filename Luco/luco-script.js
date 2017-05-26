24436047b85d78b4430e168d27a4d05eeaf2a752
Matt Albinson <mattalbinson@berkeley.net>
2017-05-26 10:20:14 -0700

    something2

--- Luco/luco-script.js
+++ Luco/luco-script.js
@@ -22,5 +22,25 @@ function build() {
             $("#weight").val(),
             $("#height").val());
         console.log(user1);
+        users.push(user1);
+        console.log(users[1].password)
     });
+}
+
+function signin(){
+    var username = document.getElementById("username1").value;
+    var password = document.getElementById("password1").value;
+    for(key in users){
+        if(users[key].username == username){
+            if(users[key].password== password){
+                window.location.assign("http://localhost:63342/BuzzQuiz/Luco/luco-index.html?_ijt=qccjpqa00bcvlr0220ftorhrhe#page4");
+            }
+            else{
+                document.getElementById("signinoutput").innerHTML = "Username and Password do not match :("
+            }
+        }
+        else{
+            document.getElementById("output").innerHTML = "Username invalid, try another or sign up"
+        }
+    }
 }
