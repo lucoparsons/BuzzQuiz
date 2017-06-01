7eb3e5df5bb43fa1793bc23234998bb46dee5cc5
Borna Karimi <bornakarimi@students.berkeley.net>
2017-06-01 09:58:02 -0700

    master

--- Borna/script.js
+++ Borna/script.js
@@ -1,8 +1,9 @@
-/**
- * Created by h205p2 on 5/18/17.
- *  test push, working?
- */
 var waterDrank = 0;
+var waterTotal = 0;
+var alcoholDrank = 0;
+var alcoholTotal = 0;
+var excerciseDo = 0;
+var excerciseTotal = 0;
 
 function User(name, pass, sex, weight, height) {
     this.username = name;
@@ -16,25 +17,17 @@ function User(name, pass, sex, weight, height) {
     this.liquidLeft = this.weight * .6;
 }
 
-function signin(){
-    var username = document.getElementById("username1").value;
-    var password = document.getElementById("password1").value;
-    for(key in users){
-        if(users[key].username == username){
-            if(users[key].password== password){
-                window.location.assign("/index.html?_ijt=qccjpqa00bcvlr0220ftorhrhe#page4");
-            }
-            else{
-                document.getElementById("signinoutput").innerHTML = "Username and Password do not match :("
-            }
-        }
-        else{
-            document.getElementById("output").innerHTML = "Username invalid, try another or sign up"
-        }
-    }
+function download(array, name) {
+    var json = JSON.stringify(array);
+    localStorage.setItem(name, json);
 }
 
-
+function reload(name) {
+    var text = localStorage.getItem(name);
+    var obj = JSON.parse(text);
+    console.log(obj);
+    return obj;
+}
 
 function build() {
     $(document).ready(function(){
@@ -45,14 +38,61 @@ function build() {
             $("#weight").val(),
             $("#height").val());
         console.log(user1);
+        users.push(user1);
+        console.log(user1.password);
+        console.log(users[1].password);
+        download(users, "userDatabase");
+        reload("userDatabase");
+        person = user1;
+        console.log(person);
     });
+
+}
+console.log(JSON.parse(localStorage.getItem("userDatabase")));
+console.log(JSON.parse(localStorage.getItem("userDatabase"))[0].username);
+
+function signin(){
+    var username = document.getElementById("username1").value;
+    var password = document.getElementById("password1").value;
+    var database = reload("userDatabase");
+    console.log(database);
+    for(key in database){
+        if(database[key].username == username){
+            if(database[key].password == password){
+                window.location.href = "#page4";
+                person = database[key];
+                console.log(person);
+            }
+            else{
+                document.getElementById("output").innerHTML = "Username and Password do not match :("
+            }
+        }
+        else{
+            document.getElementById("output").innerHTML = "Username invalid, try another or sign up"
+        }
+    }
 }
 
-build();
-
-function drinkWater() {
-    waterDrank = waterDrank + document.getElementById("waterintake").value;
+function logOut() {
+    person = "";
+    console.log(person);
+}
+function drink() {
+    waterDrank = document.getElementById("waterintake").value;
+    var waterDrink = Number(waterDrank);
+    waterTotal += waterDrink;
     document.getElementById("water").innerHTML= "";
-    document.getElementById("water").innerHTML = "Water: " + waterDrank;
+    document.getElementById("water").innerHTML = "Water: " + waterTotal;
+    alcoholDrank = document.getElementById("alcoholintake").value;
+    var alcoholDrink = Number(alcoholDrank);
+    alcoholTotal += alcoholDrink;
+    document.getElementById("alcohol").innerHTML= "";
+    document.getElementById("alcohol").innerHTML = "Alcohol: " + alcoholTotal;
+    document.getElementById("alcoholintake").value = "";
+    excerciseDone = document.getElementById("excerciseintake").value;
+    excerciseDo = Number(excerciseDone);
+    excerciseTotal += excerciseDo;
+    document.getElementById("excercise").innerHTML= "";
+    document.getElementById("excercise").innerHTML = "Excercise: " + excerciseTotal;
+    document.getElementById("excerciseintake").value = "";
 }
-
